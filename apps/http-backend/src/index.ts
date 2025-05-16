c062a7f6e204adb1b6e1ba2b52de939d675363f5
Praver Bajaj <praverbajaj1609@gmail.com>
2025-05-17 03:04:53 +0530

    http be part

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
+import dotenv, { parse } from "dotenv";
 import { middleware } from "./middleware";
 import { JWT_SECRET } from "@repo/backend-common/config";
-import { SigninSchema , roomSchema , createUserSchema } from "@repo/common/types";
-import { prismaclient } from "@repo/db/client"
+import { SigninSchema, roomSchema, createUserSchema } from "@repo/common/types";
+import { prismaclient } from "@repo/db/client";
+import bcrypt from "bcrypt";
 
 dotenv.config();
 
@@ -12,30 +13,107 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/signup", (req, res) => {
-    // db call to create user
-    // if user already exists, return error
-    // if user is created successfully, return success
-  res.send("you are signed up");
-});
-
-app.post("/signin", (req, res) => {
-  const username = "111";
-  const password = "222";
-  if (process.env.JWT_SECRET === undefined) {
-    throw new Error("JWT_SECRET is not defined");
-  }
-  const token = jwt.sign({ username }, JWT_SECRET);
-  res.send("you are logged in" + token);
-});
-
-app.post("create-room", middleware, (req, res) => {
-    // db call to create room
-    res.json({
-        roomId: "123",
-        roomName: "room name",
+app.post("/signup", async (req: any, res: any) => {
+  try {
+    const parsedata = createUserSchema.safeParse(req.body);
+    if (!parsedata.success) {
+      return res.status(400).json({
+        message: "Invalid data",
+        errors: parsedata.error.errors,
+      });
     }
-)});
+    const { name, email, password } = parsedata.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const avatar = req.body.avatar || null;
+    const user = await prismaclient.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (user) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    } else {
+      await prismaclient.user.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+          avatar,
+        },
+      });
+    }
+    res.status(201).json({
+      message: "User created successfully",
+    });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+});
+
+app.post("/signin", async (req: any, res: any) => {
+  try {
+    const parsedata = SigninSchema.safeParse(req.body);
+    if (!parsedata.success) {
+      return res.status(400).json({
+        message: "Invalid data",
+        errors: parsedata.error.errors,
+      });
+    }
+    const { email, password } = parsedata.data;
+    const user = await prismaclient.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      return res.status(400).json({
+        message: "User not found",
+      });
+    } else {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(400).json({
+          message: "Invalid password",
+        });
+      }
+    }
+
+    const token = jwt.sign({ userId: user.id }, JWT_SECRET);
+    res.send("you are logged in " + token);
+  } catch (e) {
+    res.status(500).json({
+      message: "Internal Server Error " + e,
+    });
+    console.log("Internal Server error " + e);
+  }
+});
+
+app.post("/createroom", middleware, async (req : any, res : any ) => {
+  const parsedata = roomSchema.safeParse(req.body)
+    if (!parsedata.success) {
+      return res.status(400).json({
+        message: "Invalid data",
+        errors: parsedata.error.errors,
+      });
+    }
+    // @ts-ignore
+    const userId = req.userId
+   
+  const room = await prismaclient.room.create({
+    data : {
+      slug : parsedata.data.slug,
+      adminId : userId
+    }
+  })
+  res.json({
+    roomId: room.id,
+  });
+});
 
 app.listen(3009, () => {
   console.log("Server is running on port 3000");
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,12 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
+import { JWT_SECRET } from '@repo/backend-common/config';
 import jwt from 'jsonwebtoken';
 
 export function middleware(req: Request, res: Response, next: NextFunction) {
  const token = req.headers['authorization'] ?? " ";
- const decoded = jwt.verify(token as string, process.env.JWT_SECRET as string)
+ const decoded = jwt.verify(token, JWT_SECRET)
+ console.log(decoded)
  if (decoded) {
     // @ts-ignore
-   req.username = (decoded as any).username
+   req.userId = decoded.userId
    next();
  } else {
    res.status(401).send('Unauthorized');
